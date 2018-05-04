8a4ef5411ee3e4ed95e707e7e5907262bd05f09b
Wagner Barbosa <wsoubar@gmail.com>
2018-05-04 20:14:53 -0300

    usando array puro no lugar de observable<any []>

--- src/app/courses-list/courses-list.component.ts
+++ src/app/courses-list/courses-list.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 
+
 @Component({
   selector: 'courses-list',
   templateUrl: './courses-list.component.html',
@@ -10,16 +11,26 @@ import { Observable } from 'rxjs/Observable';
 })
 export class CoursesListComponent implements OnInit {
 
-  coursesObservable: Observable<any []>;
+  //coursesObservable: Observable<any []>;
+  cursos = [];
 
   constructor(private db: AngularFireDatabase) { }
 
   ngOnInit() {
-    this.coursesObservable = this.getCourses('/cursos');
+    //this.coursesObservable = this.getCourses('/cursos');
+    this.getCursos('/cursos');
   }
 
-  getCourses(listPath): Observable<any[]> {
-    return this.db.list(listPath).valueChanges();
+//  getCourses(listPath): Observable<any[]> {
+//    return this.db.list(listPath).valueChanges();
+//  }
+
+  getCursos(listPath) {
+    this.db.list(listPath).valueChanges().subscribe(data => {
+      console.log('Passou aqui');
+      this.cursos = data;
+      console.log(data);
+    });
   }
 
 }
