eba565603f93f15439046061ab50d94824d7de6e
Wagner Barbosa <wsoubar@gmail.com>
2018-05-15 18:15:37 -0300

    AngularFirestore collection e document

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,6 +1,6 @@
 import { Curso } from './../model/Curso';
 import { Observable } from 'rxjs/Observable';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Injectable } from '@angular/core';
 
 
